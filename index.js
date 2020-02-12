163cc92abf966251fd63cf77f436cc6f69ca39dc
QuintinMakwe <quintinmakwe@gmail.com>
2020-02-12 05:09:27 +0100

    The makePostRequest function is almost set. All the fields of the post request has been set. Next step is to figure out how to make the reques

--- index.js
+++ index.js
@@ -26,10 +26,23 @@ axios.get('https://www.freetutorialsus.com/request-course/')
             console.log(inputId);
             // Write a function to make a post request using axios
             function makePostRequest(){
-                let firstname = ["kelvin", "Quintin", "Makwe"]
-                let subFirstname, lastname, email, nameOfCourse, courselink
-                
-                 $("input").find("#wpforms-389-field_0").attr('value') = "kelvin"
+                let firstName = ['kelvin', 'quintin', 'daze','charles','jewel','kelechi'];
+                let lastName = ['makwe', 'ikuchu', 'ani', 'eze', 'cheya','chigbo'];
+                let email = ['kelvinmakwe@gmail.com', 'quintinmakwe@gmail.com', 'frankpeterani@gmail.com','ezehcharles19@gmail.com','cheyajewel@gmail.com','kelechichigbo24@gmail.com'];
+                let lengthOfLoop = firstName.length;
+                for(let i = 0; i < lengthOfLoop; i++){
+                    let fName = firstName[i];
+                    let lName = lastName[i];
+                    let rEmail = email[i];
+                    let courseName = "MERN Stack Front to Back: Full Stack React, Redux and Node js";
+                    let courseLink = "https://www.udemy.com/course/mern-stack-front-to-back/"
+                    $("input").find(inputId[0]).attr('value') = fName;
+                    $("input").find(inputId[1]).attr('value') = lName;
+                    $("input").find(inputId[2]).attr('value') = rEmail;
+                    $("input").find(inputId[3]).attr('value') = courseName;
+                    $("input").find(inputId[4]).attr('value') = courseLink;
+                }
+                 
             }
         }
     }, error => console.log(err)
